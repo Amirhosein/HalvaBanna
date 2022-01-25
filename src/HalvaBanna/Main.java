c1187ca24535b7f8bf607b750278f4bd3a3e0668
Amir Iravanimanesh <airavanimanesh@gmail.com>
2022-01-26 00:28:41 +0330

    feat: phase 0 added

--- src/HalvaBanna/Main.java
+++ src/HalvaBanna/Main.java
@@ -1,7 +1,41 @@
 package HalvaBanna;
 
+import HalvaBanna.models.Graph;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int n, m;
+        String command;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        Graph graph = new Graph(n);
+
+        for (int i = 0; i < n; i++) {
+            map.put(sc.nextInt(), i);
+        }
+        graph.setVerticesMap(map);
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            graph.addEdge(u, v, w);
+        }
+        command = sc.next();
+        while (!Objects.equals(command, "exit")) {
+            if (Objects.equals(command, "test")) {
+                // use dfs from first index
+                graph.DFS(0);
+                System.out.println();
+            }
+            command = sc.next();
+        }
     }
 }
--- src/HalvaBanna/models/Graph.java
+++ src/HalvaBanna/models/Graph.java
@@ -1,4 +1,69 @@
 package HalvaBanna.models;
 
+import java.util.HashMap;
+import java.util.Vector;
+
 public class Graph {
+    private int V, level;
+    private Vector<Integer>[] adj;
+
+    public HashMap<Integer, Integer> getVerticesMap() {
+        return verticesMap;
+    }
+
+    public void setVerticesMap(HashMap<Integer, Integer> verticesMap) {
+        this.verticesMap = verticesMap;
+    }
+
+    private HashMap<Integer, Integer> verticesMap;
+
+    @SuppressWarnings("unchecked")
+    public Graph(int V) {
+        this.V = V;
+        this.adj = new Vector[2 * V];
+        this.verticesMap = new HashMap<>();
+
+        for (int i = 0; i < 2 * V; i++) {
+            this.adj[i] = new Vector<>();
+        }
+    }
+
+    public void addEdge(int u, int v, int weight) {
+        v = getName(v);
+        u = getName(u);
+
+        this.adj[u].add(v);
+        this.adj[v].add(u);
+    }
+
+    private void DFSUtil(int v, boolean[] visited) {
+        visited[v] = true;
+        // get key of value v
+        int key = getKey(v);
+        System.out.print(key + " ");
+
+        for (int n : adj[v]) {
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
+    public void DFS(int v) {
+        boolean[] visited = new boolean[V];
+
+        DFSUtil(v, visited);
+    }
+
+    private int getName(int v) {
+        return this.verticesMap.get(v);
+    }
+
+    private int getKey(int v) {
+        for (int key : this.verticesMap.keySet()) {
+            if (this.verticesMap.get(key) == v) {
+                return key;
+            }
+        }
+        return -1;
+    }
 }
